16249ae255a49b0179ca09497be3b58c3f830510
arenneu <jisusjy@naver.com>
2021-07-27 15:48:04 +0900

    0727_GNB_슬라이드쇼_콘텐츠영역_작업

--- 풀이/script/script.js
+++ 풀이/script/script.js
@@ -1,24 +1,66 @@
-$(function(){
+$(function () {
 
     // GNB 시작(세로형1열)
-    // $(".mainLi").mouseover(function(){
-    //     $(this).children("ul.subMenu").stop().slideDown(300);
+
+    // $(".mainLi").mouseover(function () {
+    //     $(this).children("ul.subMenu").stop().slideDown(600);
     // });
 
-    // $(".mainLi").mouseout(function(){
+    // $(".mainLi").mouseout(function () {
     //     $(this).children("ul.subMenu").stop().slideUp(500);
     // });
+
     // GNB 끝(세로형1열)
-    
+
     // GNB 시작(세로형 2열)
-
-    $(".mainLi").mouseover(function(){
-        $(this).children("ul.subMenu").stop().slideDown(500);
+    $(".mainLi").mouseover(function () {
+        $(this).children("ul.subMenu").stop().fadeIn(700);
     });
 
-    $(".mainLi").mouseout(function(){
-        $(this).children("ul.subMenu").stop().fadeOut(200);
+    $(".mainLi").mouseout(function () {
+        $(this).children("ul.subMenu").stop().fadeOut(300);
     });
-
     // GNB 끝(세로형 2열)
+
+    // 슬라이드쇼 세로형 시작
+    // setInterval(fnSlide, 3000);
+
+    // function fnSlide() {
+    //     $("#shuttleFrame").animate(
+    //         { "margin-top": "-350px" },
+    //         1600,
+    //         function () {
+    //             $("#slide a:first-child").insertAfter("#slide a:last-child");
+    //             $(this).css({ "margin-top": "0" });
+    //         }
+    //     );
+    // }
+    // 슬라이드쇼 세로형 끝
+
+    // 슬라이드쇼 가로형 시작
+    // setInterval(fnSlide, 3000);
+    // function fnSlide() {
+    //     $("#shuttleFrame").animate(
+    //         { "margin-left": "-780px" },
+    //         1600,
+    //         function () {
+    //             $("#slide a:first-child").insertAfter("#slide a:last-child");
+    //             $(this).css({ "margin-left": "0" });
+    //         }
+    //     );
+    // }
+    // 슬라이드쇼 가로형 끝
+
+    // 교차전환형 시작
+    function fnSlide() {
+        $("#slide a").eq(0).fadeOut(500,
+            function () {
+                $(this).insertAfter("slide a:last-child");
+            }
+        );
+        $("#slide a").eq(1).fadeIn(1000);
+    }
+
+    // 교차전환형 끝
+
 });
